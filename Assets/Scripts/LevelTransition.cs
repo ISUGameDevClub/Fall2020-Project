3fbf8194f172f195ebc0a6a3c687480711e9da54
Jason Kress <greenmegaman33@gmail.com>
2020-10-21 21:42:36 -0500

    Basic Level Transition

--- Assets/Scripts/LevelTransition.cs
+++ Assets/Scripts/LevelTransition.cs
@@ -2,36 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class ScreenTransition : MonoBehaviour
+public class LevelTransition : MonoBehaviour
 {
     public bool fadeOut, fadeIn;
     public float fadeSpeed;
     public float a;
+    public string level;
     public Image trans;
     public GameObject act;
-    public Title title;
+   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(fadeOut == true)
+        if (fadeOut == true)
         {
             Color objectColor = trans.GetComponent<Image>().color;
             float fadeAmount = objectColor.a - (fadeSpeed - Time.deltaTime);
 
             objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
             trans.GetComponent<Image>().color = objectColor;
-            if(objectColor.a <= 0)
+            if (objectColor.a <= 0)
             {
                 fadeOut = false;
             }
-            
+
         }
 
         if (fadeIn == true)
@@ -57,7 +60,9 @@ public class ScreenTransition : MonoBehaviour
 
     public void FadeIn()
     {
+        
         act.SetActive(true);
+        
         fadeIn = true;
         if (fadeIn == true)
         {
@@ -71,15 +76,23 @@ public class ScreenTransition : MonoBehaviour
                 fadeIn = false;
             }
             StartCoroutine(Wait());
-            
+
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FadeIn();  
+     }
+
+    
 
     IEnumerator Wait()
     {
         yield return new WaitForSeconds(1);
-        title.MoveScene();
+        SceneManager.LoadScene(level);
     }
+
+    
 
 }

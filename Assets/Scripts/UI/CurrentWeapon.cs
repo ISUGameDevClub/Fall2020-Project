e6d4681b5902272522d26a07c3be430888959d8a
HongJie Lin <k87980773@gmail.com>
2020-10-25 16:27:12 -0500

    Added weapon UI

---
+++ Assets/Scripts/UI/CurrentWeapon.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentWeapon : MonoBehaviour
+{
+    public int SwitchWeapon;
+    public void Update()
+    {
+        SwitchWeaponKeyPress();
+        SwitchWeaponController();
+    }
+    private void SwitchWeaponKeyPress()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SwitchWeapon = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+
+            SwitchWeapon = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+
+            SwitchWeapon = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+
+            SwitchWeapon = 3;
+        }
+    }
+    private void SwitchWeaponController()
+    {
+        if (Input.GetAxisRaw("Mouse ScrollWheel") > 0 && SwitchWeapon < 3)
+        {
+            SwitchWeapon += 1;
+
+        }
+        else if (Input.GetAxisRaw("Mouse ScrollWheel") < 0 && SwitchWeapon > 0)
+        {
+            SwitchWeapon -= 1;
+
+        }
+    }
+}
---
+++ Assets/Scripts/UI/SwitchWeapon.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Globalization;
+
+public class SwitchWeapon : MonoBehaviour
+{
+    private WeaponInventory wi;
+    private CurrentWeapon cw;
+    private PlayerMovement pm;
+    public GameObject WeaponBox;
+    public GameObject Attack;
+    public string WeaponType;
+    public int CurrentWeapon;
+    
+    public void Start()
+    {
+        wi = FindObjectOfType<WeaponInventory>();
+        cw = GameObject.FindGameObjectWithTag("Player").GetComponent<CurrentWeapon>();
+        pm = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        CurrentWeapon = cw.SwitchWeapon;
+        
+        AddWeapon(CurrentWeapon);
+
+
+
+    }
+    private void AddWeapon(int slot)
+    {
+        if(wi.weapons[slot] == WeaponType)
+        {
+            pm.curAttack = WeaponBox;
+        }
+        
+    }
+    
+
+
+}
